3aba7e391ccfbce78ff5ddbc90954ba215c68e49
KillyBOT <edwards.kyle.j@gmail.com>
2025-02-19 13:26:14 -0500

    Collisions working! Now on to the main game

--- highlight.gd
+++ highlight.gd
@@ -1,8 +1,8 @@
-extends Node2D
+extends Control
 
 @export var radius : float = 10.0
 @export var color : Color = Color.RED
 @export var width : float = 1.0
 
 func _draw():
-	draw_circle(Vector2.ZERO, radius, color, false, width)
+    draw_circle(Vector2.ZERO, radius, color, false, width)
--- root_system.gd
+++ root_system.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-@export var max_root_click_dist_squared = 1250.0
+signal game_over
+
+@export var max_root_click_dist = 30.0
 @export var split_button = MOUSE_BUTTON_LEFT
 @export var stop_button = MOUSE_BUTTON_RIGHT
 @export var growth_speed: float = 100.0  # Pixels per second
@@ -12,7 +14,9 @@ var right_direction = Vector2.DOWN.rotated(-split_angle)
 var _do_split : bool = false
 var _do_stop : bool = false
 
-@onready var _highlight: Node2D = $highlight
+@onready var running : bool = true
+
+@onready var _highlight: Control = $highlight
 @onready var _camera: Camera2D = $camera
 
 @onready var num_roots_growing = 1
@@ -21,56 +25,72 @@ var _do_stop : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$init_root.direction = left_direction
-	$init_root.parent = self
-	
+    var init_root = $init_root
+
+    init_root.direction = left_direction
+    init_root.parent = self
+    init_root.collider.shape.a = position + left_direction
+    init_root.collider.shape.b = position + left_direction
+    init_root.collider.disabled = false
+
 func _input(event):
-	if event is InputEventMouseButton and event.pressed:
-		match event.button_index:
-			split_button:
-				_do_split = true
-			stop_button:
-				_do_stop = true
+    if event is InputEventMouseButton and event.pressed:
+        match event.button_index:
+            split_button:
+                _do_split = true
+            stop_button:
+                _do_stop = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	# Update closest root
-	var mouse_pos = get_local_mouse_position()
-	var closest_root = null
-	var split_pos = null
-	var min_dist_from_mouse = max_root_click_dist_squared
+    # Update closest root
+    var mouse_pos = get_local_mouse_position()
+    var closest_root = null
+    var split_pos = null
+    var min_dist_from_mouse = max_root_click_dist * max_root_click_dist
 
-	for root in get_children():
-		if root is Area2D:
-			var p : Vector2 = root.orthogonal_projection(mouse_pos)
-			var dist_from_mouse = p.distance_squared_to(mouse_pos)
-			
-			if root.point_on_root(p) and dist_from_mouse < min_dist_from_mouse:
-				split_pos = p
-				min_dist_from_mouse = dist_from_mouse
-				closest_root = root
+    for root in get_children():
+        if root is Area2D:
+            var p : Vector2 = root.orthogonal_projection(mouse_pos)
+            var dist_from_mouse = p.distance_squared_to(mouse_pos)
+            
+            if root.point_on_root(p) and dist_from_mouse < min_dist_from_mouse:
+                split_pos = p
+                min_dist_from_mouse = dist_from_mouse
+                closest_root = root
+        
+            if root.growing:
+                max_root_depth = max(max_root_depth, root.line.points[-1][1])
+            elif not root.is_on_screen(_camera.position[1] - _half_screen_height):
+                root.queue_free()
+            _camera.position[1] = max_root_depth
 
-	_highlight.visible = closest_root != null
-	if closest_root != null:
-		_highlight.position = split_pos
-		#_highlight.queue_redraw()
+    _highlight.visible = closest_root != null
+    if closest_root != null:
+        _highlight.position = split_pos
 
-	if _do_split and closest_root != null:
-		closest_root.split_at(split_pos)
-	
-	if _do_stop and closest_root != null:
-		closest_root.stop_growing()
+    if running:
+        if _do_split and closest_root != null:
+            _do_split = false
+            closest_root.split_at(split_pos)
+        
+        if _do_stop and closest_root != null:
+            _do_stop = false
+            closest_root.stop_growing(true)
+    
+        if num_roots_growing == 0:
+            game_over.emit()
 
-	# Remove any queued events
-	_do_split = false
-	_do_stop = false
-	
-	for root in get_children():
-		if root is Area2D:
-			if root.is_growing:
-				max_root_depth = max(max_root_depth, root.line.points[-1][1])
-			elif not root.is_on_screen(_camera.position[1] - _half_screen_height):
-				root.queue_free()
-	_camera.position[1] = max_root_depth
-	if num_roots_growing == 0:
-		print("Game over! Root depth: ", _camera.position[1])
+func _on_root_hit() -> void:
+    game_over.emit()
+
+func _on_game_over() -> void:
+    if not running:
+        return
+
+    running = false
+    if num_roots_growing > 0:
+        for root in get_children():
+            if root is Area2D:
+                root.stop_growing(false)
+    print("Game over! Root depth: ", max_root_depth)
